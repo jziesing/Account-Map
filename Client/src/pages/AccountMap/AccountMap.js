485e8ca801f4610b21bc2e78f512f1cc782c59d0
Jack Ziesing <jziesing@gmail.com>
2019-01-18 11:03:25 -0800

    changing hover color to gray

--- Client/src/pages/AccountMap/AccountMap.js
+++ Client/src/pages/AccountMap/AccountMap.js
@@ -161,8 +161,8 @@ class AccountMap extends React.Component {
                                             outline: "none",
                                           },
                                           hover: {
-                                            fill: "#79589f",
-                                            stroke: "#79589f",
+                                            fill: "#607D8B",
+                                            stroke: "#607D8B",
                                             strokeWidth: 0.75,
                                             outline: "none",
                                           },
